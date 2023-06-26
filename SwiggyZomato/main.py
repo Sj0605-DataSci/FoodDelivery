00e1f4ca6288e6ec83b064739cf7cbaca3039491
Sanyam Jain <64300568+Sj0605-DataSci@users.noreply.github.com>
2023-06-26 18:45:07 +0530

    commit

--- SwiggyZomato/main.py
+++ SwiggyZomato/main.py
@@ -10,12 +10,6 @@ data = pd.read_csv("SwiggyZomato/data.txt")
 from keras.models import load_model
 
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-local_css("SwiggyZomato/style/style.css")
-
 model = load_model("SwiggyZomato/model.h5")
 
 _, col2, _ = st.columns([1, 2, 1])
